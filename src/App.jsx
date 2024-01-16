6fc17fa7ee63d0ac35f59e830573c0abca0806e7
hyperst8 <huypn82@gmail.com>
2024-01-16 19:48:54 +0100

    Move import images to separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import image01 from "./assets/01.jpg";
-import image02 from "./assets/02.jpg";
-import image03 from "./assets/03.jpg";
-import image04 from "./assets/04.jpg";
-import image05 from "./assets/05.jpg";
-import image06 from "./assets/06.jpg";
-
+// Import components
 import SliderImages from "./components/SliderImages";
 
-function App() {
-  const images = [image01, image02, image03, image04, image05, image06];
+// Import content
+import images from "./content/data";
 
+function App() {
   return (
     <div className="app">
       <SliderImages images={images} />
---
+++ src/content/data.js
@@ -0,0 +1,8 @@
+import image01 from "../assets/01.jpg";
+import image02 from "../assets/02.jpg";
+import image03 from "../assets/03.jpg";
+import image04 from "../assets/04.jpg";
+import image05 from "../assets/05.jpg";
+import image06 from "../assets/06.jpg";
+
+export const images = [image01, image02, image03, image04, image05, image06];
