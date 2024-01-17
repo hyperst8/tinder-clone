86ad24cf7b97642d3b182fec8098819bdacf00ba
hyperst8 <huypn82@gmail.com>
2024-01-17 11:51:03 +0100

    Add alternative images

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import Slider from "./components/Slider";
 
 // Import content
 import handiCrafts from "./content/data.js";
+// import foodImages from "./content/foody.js";
 
 function App() {
   return (
     <div className="app">
-      {/* <SliderImages images={images} /> */}
+      {/* <SliderImages images={foodImages} /> */}
       <div className="max-w-lg h-dvh flex flex-col items-center justify-evenly mx-auto">
         <div>
           <h1 className="font-bold text-2xl drop-shadow-lg">Tinder Clone</h1>
